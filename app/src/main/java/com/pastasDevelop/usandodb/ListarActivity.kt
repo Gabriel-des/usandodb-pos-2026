58d0b497a404d00dcd47d1ab0d7ae52943285c78
Gabriel Vieira <gabivs2000@gmail.com>
2026-06-13 14:00:20 -0300

    Creating a list with a data from database using simple cursor adapter

--- app/src/main/java/com/pastasDevelop/usandodb/ListarActivity.kt
+++ app/src/main/java/com/pastasDevelop/usandodb/ListarActivity.kt
@@ -2,15 +2,19 @@ package com.pastasDevelop.usandodb
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.SimpleCursorAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.pastasDevelop.usandodb.database.DatabaseHandler
 import com.pastasDevelop.usandodb.databinding.ActivityListarBinding
+import com.pastasDevelop.usandodb.entity.Cadastro
 
 class ListarActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityListarBinding
+    private lateinit var db: DatabaseHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +29,19 @@ class ListarActivity : AppCompatActivity() {
             insets
         }
 
-        val registros = listOf("Brasil", "Argentina", "Chile")
+        db = DatabaseHandler(this)
 
-        val adpater = ArrayAdapter<String>(
+        val registros = db.listarCursor()
+
+        val adapter = SimpleCursorAdapter(
             this,
-            android.R.layout.simple_list_item_1,
-            registros
+            android.R.layout.simple_list_item_2,
+            registros,
+            arrayOf("nome", "telefone"),
+            intArrayOf(android.R.id.text1, android.R.id.text2),
+            0
         )
 
-        binding.lvCadastro.adapter = adpater
+        binding.lvCadastro.adapter = adapter
     }
 }
